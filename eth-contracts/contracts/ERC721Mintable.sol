8f78f8217738f56e980152968b5a0e1afdf3daa9
Mattia-Code <mattia.piparo@gmail.com>
2020-05-02 18:24:35 +0200

    :tada: finito ownable

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -9,15 +9,43 @@ import "./Oraclize.sol";
 contract Ownable {
     //  TODO's
     //  1) create a private '_owner' variable of type address with a public getter function
-    //  2) create an internal constructor that sets the _owner var to the creater of the contract 
+    address private _owner;
+    /// Look up the address of the owner
+    function owner() public view returns (address) {
+        return _owner;
+    }
+    //  2) create an internal constructor that sets the _owner var to the creater of the contract
+    // Assign the contract to an owner
+    constructor () internal {
+        _owner = msg.sender;
+        emit TransferOwnership(address(0), _owner);
+    }
     //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
+    /// Check if the calling address is the owner of the contract
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+    /// Define a function modifier 'onlyOwner'
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
     //  4) fill out the transferOwnership function
     //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    // Define an Event
+    event TransferOwnership(address indexed oldOwner, address indexed newOwner);
+
+    /// Define a public function to transfer ownership
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
 
     function transferOwnership(address newOwner) public onlyOwner {
         // TODO add functionality to transfer control of the contract to a newOwner.
         // make sure the new owner is a real address
-
+        require(newOwner != address(0));
+        emit TransferOwnership(_owner, newOwner);
+        _owner = newOwner;
     }
 }
 
